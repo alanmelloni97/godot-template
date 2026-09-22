8296b759342555c99e706b12b501f197fc8def11
Alan <alanmel97@gmail.com>
2026-09-23 00:28:07 +0200

    updated animated progress bar

--- game_logic/components/ui/animated_progress_bar/animated_progress_bar.gd
+++ game_logic/components/ui/animated_progress_bar/animated_progress_bar.gd
@@ -1,7 +1,7 @@
 extends TextureProgressBar
 class_name AnimatedProgressBar
 
-@export var low_value_anim_percent: int = 20
+@export var low_value_anim_percent: float = 0.2
 @export var anim_time: float = 0.3
 var tween: Tween
 var initial_progress_tint: Color
@@ -14,8 +14,14 @@ func _ready() -> void:
 
 
 func _on_value_changed(bar_value: float) -> void:
-	if bar_value < low_value_anim_percent:
+	if bar_value < (max_value * low_value_anim_percent):
 		animate_low_value_bar()
+		print("Bar value is below low value threshold, starting animation.")
+	else:
+		if tween:
+			tween.kill()
+		tint_progress = initial_progress_tint
+		print("Bar value is above low value threshold, stopping animation and resetting tint.")
 
 
 func animate_low_value_bar() -> void:
